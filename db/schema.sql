1ad86b1748be344168b09a7475ffc7aebf0d2915
kateleighb3 <kbaldw3@gmail.com>
2023-08-03 19:18:52 -0400

    add server.js and database

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO department (dept_name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal"),
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 1000000, 1),
+       ("Salesperson", 80000, 1),
+       ("Lead Engineer", 150000, 2),
+       ("Software Engineer", 120000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
+       
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Stevie", "Nicks", 1, NULL),
+       ("Britney", "Spears", 1, 1),
+       ("Tommy", "Lee", 2, NULL),
+       ("Dr.", "Dre", 2, 3),
+       ("Snoop", "Dogg", 3, NULL),
+       ("Lil", "Kim", 3, 5),
+       ("Willy", "Nelson", 4, NULL),
+       ("Mariah", "Carey", 4, 7);
+       
