214456db44295281806e9e38177a4b4ec5dfd09b
kateleighb3 <kbaldw3@gmail.com>
2023-08-06 20:26:38 -0400

    add to server.js

--- server.js
+++ server.js
@@ -62,9 +62,254 @@ function initialPrompt() {
                 exit();
             }
         })
-      }
+      };
 
 initialPrompt();
+
+function viewAllEmployees() {
+    connection.query(`SELECT employee.first_name, 
+            employee.last_name,
+            roledb.title AS Title,
+            roledb.salary AS Salary,
+            department.dept_name AS Department
+        FROM employee 
+            INNER JOIN roledb ON employee.roledb_id=roledb.roledb_id
+            INNER JOIN department ON employee.roledb_id=department.department_id`, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+
+    })
+    initialPrompt();
+}
+
+function viewByDept() {
+    inquirer.prompt([{
+        message: "Which department would you like to view?",
+        type: 'list',
+        name: 'dept',
+        choices: [
+            'Engineering',
+            'Management',
+            'Marketing',
+            'Legal',
+            'Sales'
+        ]
+    }])
+        .then((answers) => {
+            connection.query(`SELECT 
+            employee.first_name,
+            employee.last_name,
+            department.dept_name AS Department
+        FROM employee
+            INNER JOIN roledb ON employee.roledb_id=roledb.roledb_id     
+            INNER JOIN department ON employee.roledb_id=department.department_id
+            WHERE department.dept_name='${answers.dept}'`, (err, result) => {
+                if (err) throw err;
+                console.table(result);
+
+            })
+            initialPrompt();
+        })
+
+}
+
+function viewByRole() {
+    inquirer.prompt([{
+        message: "Which roles would you like to view?",
+        type: 'list',
+        name: 'role',
+        choices: [
+            'Junior Developer',
+            'Senior Developer',
+            'Manager',
+            'Sales Lead',
+            'Lawyer',
+            'Designer'
+        ]
+    }])
+        .then((answers) => {
+            connection.query(`SELECT 
+            employee.first_name,
+            employee.last_name,
+            roledb.title AS Title
+        FROM employee
+            INNER JOIN roledb ON employee.roledb_id=roledb.roledb_id     
+            INNER JOIN department ON employee.roledb_id=department.department_id
+            WHERE roledb.title='${answers.role}'`, (err, result) => {
+                if (err) throw err;
+                console.table(result);
+
+            })
+            initialPrompt();
+        })
+
+}
+
+function addEmployee() {
+
+    let roles = [
+        'Junior Developer',
+        'Senior Developer',
+        'Manager',
+        'Sales Lead',
+        'Lawyer',
+        'Designer'
+    ];
+
+    inquirer.prompt([
+        {
+            message: "What would be the employee's role?",
+            type: 'list',
+            name: 'role',
+            choices: roles
+        },
+        {
+            message: "What is the employees first name?",
+            type: 'input',
+            name: 'firstName',
+        },
+        {
+            message: "What is the employees last name?",
+            type: 'input',
+            name: 'lastName',
+        },
+
+    ])
+        .then((answers) => {
+
+            connection.query(`
+             INSERT INTO employee SET ?`,
+                {
+                    first_name: answers.firstName,
+                    last_name: answers.lastName,
+                    roledb_id: roles.indexOf(answers.role) + 1 
+                },
+
+                (err, result) => {
+                    if (err) throw err;
+                })
+            console.log(answers.role);
+
+        })
+    initialPrompt();
+
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+            message: "What is the name of the role you wish to add?",
+            type: 'input',
+            name: 'newRole'
+        },
+        {
+            message: "What is the yearly salary of the role?",
+            type: 'input',
+            name: 'salary'
+        },
+    ]).then((answers) => {
+        connection.query(`
+             INSERT INTO roledb SET ?`,
+            {
+                title: answers.newRole,
+                salary: answers.salary,
+            },
+            (err, result) => {
+                if (err) throw err;
+
+            })
+
+    })
+    exit();
+}
+
+function addDept() {
+    inquirer.prompt([
+        {
+            message: "What is the name of the Department?",
+            type: 'input',
+            name: 'newDept'
+        },
+    ])
+        .then(answers => {
+            connection.query(`
+             INSERT INTO department SET ?`,
+                {
+                    name: answers.newDept,
+                },
+                (err, result) => {
+                    if (err) throw err;
+
+                })
+
+        })
+    exit();
+}
+
+function updateEmployeeRole() {
+    let employees = [
+        'Jerry Seinfeld',
+        'George Constanza',
+        'Elaine Benes',
+        'Cosmo Kramer',
+        'Rachel Green',
+        'Phoebe Boufay',
+        'Chandler Bing',
+        'Joey Tribiani',
+        'Ross Gellar',
+        'Monica Gellar'
+    ];
+
+    let roles = [
+        'Junior Developer',
+        'Senior Developer',
+        'Manager',
+        'Sales Lead',
+        'Lawyer',
+    ];
+
+    inquirer.prompt([
+        {
+            message: "Which Employee do you wish to update?",
+            type: 'list',
+            name: 'employee',
+            choices: employees
+        },
+        {
+            message: "What is his/her new role?",
+            type: 'list',
+            name: 'role',
+            choices: roles
+        }
+    ])
+        .then(answers => {
+            connection.query(`
+            UPDATE employee_DB.employee 
+            SET roledb_id = '${roles.indexOf(answers.role) + 1}' WHERE (employee_id = '${employees.indexOf(answers.employee) + 1}');`,
+                (err, result) => {
+                    if (err) throw err;
+                    console.log(result);
+                })
+
+        })
+
+}
+
+function viewRoles() {
+    connection.query(`SELECT * FROM employee_DB.roledb`, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+    })
+    initialPrompt();
+}
+
+function viewDepts() {
+    connection.query(`SELECT * FROM employee_DB.department`, (err, result) => {
+        if (err) throw err;
+        console.table(result);
+    })
+    initialPrompt();
+}
 
 function exit() {
     connection.end();
